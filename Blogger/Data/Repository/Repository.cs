5c7e812ed6509c6811d04c12252c655e97b90d2e
Sxmmie <samuelumohjnr@gmail..com>
2020-04-10 13:57:18 +0100

    Fix the issues with the pagination
    Fixed a bug in the pagination, displaying more pages than expected.

--- Blogger/Data/Repository/Repository.cs
+++ Blogger/Data/Repository/Repository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blogger.Helpers;
 using Blogger.Models;
 using Blogger.Models.Comments;
 using Blogger.ViewModels;
@@ -45,44 +46,12 @@ namespace Blogger.Data.Repository
                 PageNumber = pageNumber,
                 PageCount = pageCount,
                 NextPage = postsCount > skipAmount + pageSize,
-                Pages = PageNumbers(pageNumber, pageCount),
+                Pages = PageHelper.PageNumbers(pageNumber, pageCount).ToList(),
                 Category = category,
                 Posts = query.Skip(skipAmount).Take(pageSize).ToList()
             };
 
             return posts;
-        }
-
-        private IEnumerable<int> PageNumbers(int pageNumber, int pageCount)
-        {
-            int midPoint = pageNumber < 3 ? 3 : pageNumber > pageCount - 2 ? pageCount - 2 : pageNumber;
-
-            var lowerBound = midPoint - 2;
-            var upperBound = midPoint + 2;
-
-            if (lowerBound != 1)
-            {
-                yield return 1;
-                if (lowerBound - 1 > 1)
-                {
-                    yield return 1;
-                }
-            }
-
-            for (var i = midPoint - 2; i <= midPoint + 2; i++)
-            {
-                yield return i;
-            }
-
-            if (upperBound != pageCount)
-            {
-                if (pageCount - upperBound > 1)
-                {
-                    yield return -1;
-                }
-
-                yield return pageCount;
-            }
         }
 
         public void AddPost(Post post)
---
+++ Blogger/Helpers/PageHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogger.Helpers
+{
+    public static class PageHelper
+    {
+        public static IEnumerable<int> PageNumbers(int pageNumber, int pageCount)
+        {
+            if (pageCount <= 5)
+            {
+                for (var i = 1; i <= pageCount; i++)
+                {
+                    yield return i;
+                }
+            }
+            else
+            {
+
+                int midPoint = pageNumber < 3 ? 3 : pageNumber > pageCount - 2 ? pageCount - 2 : pageNumber;
+
+                var lowerBound = midPoint - 2;
+                var upperBound = midPoint + 2;
+
+                if (lowerBound != 1)
+                {
+                    yield return 1;
+                    if (lowerBound - 1 > 1)
+                    {
+                        yield return 1;
+                    }
+                }
+
+                for (var i = midPoint - 2; i <= midPoint + 2; i++)
+                {
+                    yield return i;
+                }
+
+                if (upperBound != pageCount)
+                {
+                    if (pageCount - upperBound > 1)
+                    {
+                        yield return -1;
+                    }
+
+                    yield return pageCount;
+                }
+            }
+        }
+    }
+}
