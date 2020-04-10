27b8e0e1aa7bbf1ba77011f48e042c304ab255c2
Sxmmie <samuelumohjnr@gmail..com>
2020-04-10 13:04:25 +0100

    Implemented smart pagination

--- Blogger/Data/Repository/Repository.cs
+++ Blogger/Data/Repository/Repository.cs
@@ -38,17 +38,51 @@ namespace Blogger.Data.Repository
                 query = query.Where(x => InCategory(x));
 
             var postsCount = query.Count();
+            var pageCount = (int)Math.Ceiling((double)postsCount / pageSize);
 
             var posts = new IndexViewModel
             {
                 PageNumber = pageNumber,
-                PageCount = (int) Math.Ceiling((double)postsCount / pageSize),
+                PageCount = pageCount,
                 NextPage = postsCount > skipAmount + pageSize,
+                Pages = PageNumbers(pageNumber, pageCount),
                 Category = category,
                 Posts = query.Skip(skipAmount).Take(pageSize).ToList()
             };
 
             return posts;
+        }
+
+        private IEnumerable<int> PageNumbers(int pageNumber, int pageCount)
+        {
+            int midPoint = pageNumber < 3 ? 3 : pageNumber > pageCount - 2 ? pageCount - 2 : pageNumber;
+
+            var lowerBound = midPoint - 2;
+            var upperBound = midPoint + 2;
+
+            if (lowerBound != 1)
+            {
+                yield return 1;
+                if (lowerBound - 1 > 1)
+                {
+                    yield return 1;
+                }
+            }
+
+            for (var i = midPoint - 2; i <= midPoint + 2; i++)
+            {
+                yield return i;
+            }
+
+            if (upperBound != pageCount)
+            {
+                if (pageCount - upperBound > 1)
+                {
+                    yield return -1;
+                }
+
+                yield return pageCount;
+            }
         }
 
         public void AddPost(Post post)
