638ef8a90c85ab585f695b59a51baeaf4359e17b
Sxmmie <samuelumohjnr@gmail..com>
2020-04-10 19:42:50 +0100

    Implemented the sign up feature
    Added sign up and authenticating user. Also a user  an only make comments only if authenticated.

--- Blogger/Controllers/AuthController.cs
+++ Blogger/Controllers/AuthController.cs
@@ -11,10 +11,12 @@ namespace Blogger.Controllers
     public class AuthController : Controller
     {
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public AuthController(SignInManager<IdentityUser> signInManager)
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -28,7 +30,47 @@ namespace Blogger.Controllers
         {
             var result = await _signInManager.PasswordSignInAsync(loginViewModel.UserName, loginViewModel.Password, false, false);
 
-            return RedirectToAction("Index", "Admin");
+            if (!result.Succeeded)
+                return View(loginViewModel);
+
+            var user = await _userManager.FindByNameAsync(loginViewModel.UserName);
+            var isAdmin = await _userManager.IsInRoleAsync(user, "Admin");
+
+            if(isAdmin)
+                return RedirectToAction("Index", "Admin");
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View(new RegisterViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var user = new IdentityUser
+            {
+                UserName = registerViewModel.Email,
+                Email = registerViewModel.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, "password");
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(registerViewModel);
         }
 
         public async Task<IActionResult> Logout()
---
+++ Blogger/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogger.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
